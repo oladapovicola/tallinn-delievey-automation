30146b36bbc4f11ffa7921a10c16af92c4354b3e
oladapo.aiyenitaju <dapssyola@gmail.com>
2025-08-14 15:47:44 +0300

    Initial commit

---
+++ tests/authorization.spec.ts
@@ -0,0 +1,30 @@
+require('dotenv').config();
+
+import { test, expect } from '@playwright/test';
+import { faker } from '@faker-js/faker';
+
+const randomUsername= faker.internet.username();
+const randomPassword= faker.internet.password();
+
+
+const appUrl = process.env.APP_URL;
+
+test.only('Sign-in button is enabled', async ({ page }) => {
+  await page.goto(appUrl);
+  await page.getByTestId('username-input').fill ('randomUsername');
+  await page.getByTestId('password-input').fill ('randomPassword');
+  await expect(page.getByTestId('signIn-button')).toBeEnabled()
+
+
+});
+
+test('incorrect login credentials', async ({ page }) => {
+  await page.goto(appUrl);
+  await page.getByTestId('username-input').fill ('randomUsername');
+  await page.getByTestId('password-input').fill ('randomPassword');
+  await page.getByTestId('signIn-button').click ();
+  await expect(page.getByTestId('authorizationError-popup')).toBeVisible()
+
+
+});
+
